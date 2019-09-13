09049983e22f2854bbc5d0d364ed1a13e540e231
Sage Elliott <hello@sageelliott.com>
2019-09-13 16:54:02 -0700

    create letnet training file for mnist

--- mnist_lenet.py
+++ mnist_lenet.py
@@ -7,3 +7,64 @@ from sklearn.metrics import classification_report
 from keras import backend as K
 import matplotlib.pyplot as plt
 import numpy as np
+
+# get mnisy dataset from keras 
+print("[INFO] accessing MNIST...")
+((trainData, trainLabels), (testData, testLabels)) = mnist.load_data()
+
+# if we are using "channels first" ordering, then reshape the
+# design matrix such that the matrix is:
+# num_samples x depth x rows x columns
+if K.image_data_format() == "channels_first":
+    trainData = trainData.reshape((trainData.shape[0], 1, 28, 28))
+    testData = testData.reshape((testData.shape[0], 1, 28, 28))
+
+    # otherwise, we are using "channels last" ordering, so the design
+    # matrix shape should be: num_samples x rows x columns x depth
+else:
+    trainData = trainData.reshape((trainData.shape[0], 28, 28, 1))
+    testData = testData.reshape((testData.shape[0], 28, 28, 1))
+
+# scale data to the range of [0, 1]
+trainData = trainData.astype("float32") / 255.0
+testData = testData.astype("float32") / 255.0
+
+# convert the labels from integers to vectors
+le = LabelBinarizer()
+trainLabels = le.fit_transform(trainLabels)
+testLabels = le.transform(testLabels)
+
+# initialize the optimizer and model
+print("[INFO] compiling model...")
+opt = SGD(lr=0.01)
+model = LeNet.build(width=28, height=28, depth=1, classes=10)
+model.compile(loss="categorical_crossentropy", optimizer=opt,
+                metrics=["accuracy"])
+
+# train the network
+print("[INFO] training network...")
+H = model.fit(trainData, trainLabels,
+                validation_data=(testData, testLabels),
+                batch_size=128,
+                epochs=20,
+                verbose=1)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testData, batch_size=128)
+print(classification_report(testLabels.argmax(axis=1),
+                            predictions.argmax(axis=1),
+                            target_names=[str(x) for x in le.classes_]))
+
+# plot the training loss and accuracy
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(np.arange(0, 20), H.history["loss"], label="train_loss")
+plt.plot(np.arange(0, 20), H.history["val_loss"], label="val_loss")
+plt.plot(np.arange(0, 20), H.history["acc"], label="train_acc")
+plt.plot(np.arange(0, 20), H.history["val_acc"], label="val_acc")
+plt.title("Training Loss and Accuracy")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend()
+plt.show()
